a21a03974888ed09a86c1c92e51a29544a674738
Hamza Farooq <hamzafarooq787@gmail.com>
2026-01-04 18:37:47 +0500

    Fix Header import and siteConfig properties

--- app/services/run-flat-tyre-fitting/page.tsx
+++ app/services/run-flat-tyre-fitting/page.tsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import ServiceDetailPage from "@/components/ServiceDetailPage";
 import { siteConfig } from "@/config/site";
 
--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,9 @@
 export const siteConfig = {
+  name: "Mobile Tyre Service",
   brandColor: "#7CFC00",
   links: {
     telLink: "tel:+447412840023",
     whatsappLink: "https://wa.me/447412840023",
+    email: "info@mobiletyreservice.co.uk",
   },
 };
